8fb63e763518bfa1bc586182c5f8139e461d71fc
Breno Raeder <brenoraeder97@gmail.com>
2023-06-02 21:48:45 -0300

    segunda aula, intro ao react

---
+++ my-app/src/BotaoNum.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+function BotaoNum () {
+    const [num, setNum] = useState(0);
+    const somaNum = () => {
+        setNum(num + 1);
+    }
+    const subtraiNum = () => {
+        setNum(num - 1);
+    }
+
+    return (
+        <div className="btn-num">
+            <p>Olha um número: {num}</p>
+            <div>
+                <button onClick={subtraiNum}>-</button>
+                <button onClick={somaNum}>+</button>
+            </div>
+        </div>
+    )
+}
+
+export default BotaoNum;
---
+++ my-app/src/Component.js
@@ -0,0 +1,9 @@
+function Component() {
+    return (
+        <div>
+            <p>Componente</p>
+        </div>
+    )
+}
+
+export default Component;
